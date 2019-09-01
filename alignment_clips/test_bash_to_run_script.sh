d2dc027c436814b4b0b85cb0473838b88e059b8f
Hackhard <34208125+Hackhard@users.noreply.github.com>
2019-09-01 20:05:42 +0530

    Update test_bash_to_run_script.sh

--- alignment_clips/test_bash_to_run_script.sh
+++ alignment_clips/test_bash_to_run_script.sh
@@ -1,22 +1,18 @@
 file_dir=$1'_tmp'
 END=`wc -l $1 | awk '{print $1}'`
 END=`expr $END + 1`
-python3 e_idgenerator.py $file_dir
-python3 h_idgenerator.py $file_dir
-python3 anchors_csv_final.py $file_dir
+python3 $HOME_alignment/alignment_clips/anchors_csv_final.py $1
 for i in $(seq 1 $END)
 do
         sentence_dir='2.'$i
         echo $sentence_dir
-        tmp_path=$HOME/Downloads/BUgol_27_aug/$1_tmp/$sentence_dir
+        tmp_path=$HOME_anu_tmp/tmp/$1_tmp/$sentence_dir
 
         cd $tmp_path
 echo "(defglobal ?*hpath* = $HOME_alignment)" > $tmp_path/new_alignment.clp
 
-     myclips -f  /home/hackhard/prgrams/anchor_clp_run.bat >  $tmp_path/clips_error_allingnment
-        echo "EXXX"
-        python3 /home/hackhard/prgrams/tocsv_converter.py  $tmp_path
-        echo "dafad"
+     myclips -f  $HOME_alignment/alignment_clips/anchor_clp_run.bat >  $tmp_path/clips_error_alignment
+        python3 $HOME_alignment/alignment_clips/tocsv_converter.py  $tmp_path
 
 
 done
