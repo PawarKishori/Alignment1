1fb4c6219fb36317f16e2c969b9a46692a052218
PawarKishori <kishori.pawar@research.iiit.ac.in>
2019-09-04 17:49:43 +0530

    fixed path bug

--- tech_dict/run_tech-dict.sh
+++ tech_dict/run_tech-dict.sh
@@ -1,7 +1,7 @@
 #Prepocessing for canonical form
-sh create_canonical.sh $2
+sh tech_dict/create_canonical.sh $2
 echo "$2_canonical created"
-sh create_canonical.sh $3
+sh tech_dict/create_canonical.sh $3
 echo "$2_canonical created"
 #Calling the code for Technical_Dictionary_Integration
 python $HOME_alignment/tech_dict/Technical_Dictionary_Integration.py $1 $2_canonical $3_canonical
