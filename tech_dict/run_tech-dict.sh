05a05fd6f88b561d9588920eac95a636f176a484
nupur-thakur <nupur@nupur-HP-Pavilion-Laptop-15-cc1xx>
2019-09-21 14:46:40 +0530

    Changed the wronng message in shell file

--- tech_dict/run_tech-dict.sh
+++ tech_dict/run_tech-dict.sh
@@ -2,7 +2,7 @@
 sh tech_dict/create_canonical.sh $2
 echo "$2_canonical created"
 sh tech_dict/create_canonical.sh $3
-echo "$2_canonical created"
+echo "$3_canonical created"
 #Calling the code for Technical_Dictionary_Integration
 python $HOME_alignment/tech_dict/Technical_Dictionary_Integration.py $1 $2_canonical $3_canonical
 echo "Created Dictionary"
