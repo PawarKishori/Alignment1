1fe5da657e989c83116f9afc5afe6112d9eb847b
PawarKishori <kishori.pawar@research.iiit.ac.in>
2019-10-16 10:40:37 +0530

    new alternative code for generating facts from csv

--- alignment_clips/test_bash_to_run_script.sh
+++ alignment_clips/test_bash_to_run_script.sh
@@ -6,18 +6,18 @@ END=`expr $END + 1`
 
 python3 $HOME_alignment/alignment_clips/e_idgenerator.py $1
 python3 $HOME_alignment/alignment_clips/h_idgenerator.py $1
-python3 $HOME_alignment/alignment_clips/pandas_generalised.py $1
-#python3 $HOME_alignment/alignment_clips/anchors_csv_final.py $1
+#python3 $HOME_alignment/alignment_clips/pandas_generalised.py $1    #Old code Apratim
 for i in $(seq 1 $END)
 do
         sentence_dir='2.'$i
         echo $sentence_dir
         tmp_path=$HOME_anu_tmp/tmp/$1_tmp/$sentence_dir
+        python3 $HOME_alignment/alignment_clips/Generating_Anchor_Facts.py $1 "2."$i    #Few changes by Jagrati
 
         cd $tmp_path
-		echo "(defglobal ?*hpath* = $HOME_alignment)" > $tmp_path/new_alignment.clp
+        echo "(defglobal ?*hpath* = $HOME_alignment)" > $tmp_path/new_alignment.clp
 
-     	myclips -f  $HOME_alignment/alignment_clips/anchor_clp_run.bat >  $tmp_path/clips_error_alignment
+        myclips -f  $HOME_alignment/alignment_clips/anchor_clp_run.bat >  $tmp_path/clips_error_alignment
         python3 $HOME_alignment/alignment_clips/tocsv_converter.py  $tmp_path
 
 done
