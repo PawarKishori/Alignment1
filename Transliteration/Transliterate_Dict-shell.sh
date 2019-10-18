a3ed43a6d1e48b4408b783283e4fe4824348da9b
nupur-thakur <nupur.s.thakur@gmail.com>
2019-10-18 12:20:12 +0530

    Changes made in 2 files Transliteration_Dict.py and Transliteration_Dict-shell.sh NOTE : Changes are not tested because of the absence of missing dependencies of corpus

--- Transliteration/Transliterate_Dict-shell.sh
+++ Transliteration/Transliterate_Dict-shell.sh
@@ -1,5 +1,5 @@
-source activate python2.7
-python $HOME_alignment/Transliteration/Transliterate_Dict.py $1 $2  
-conda deactivate
+#source activate python2.7
+python3 $HOME_alignment/Transliteration/Transliterate_Dict.py $1 $2  
+#conda deactivate
 
 python3 $HOME_alignment/Transliteration/Acronym_Dict.py $1 $2
