a5fbc72a09fb409e4e8a4cba96860ea902005030
nupur-thakur <nupur@nupur-HP-Pavilion-Laptop-15-cc1xx>
2019-10-19 17:08:32 +0530

    Changes in All_Resources_id_word.py due to 3 4/3 in ai2E 2.2 sent

--- working_debug/All_Resources_id_word.py
+++ working_debug/All_Resources_id_word.py
@@ -13,6 +13,10 @@ sent_no = sys.argv[2]
 sent_dir = tmp_path + eng_file_name + "_tmp/" + sent_no
 e_file=sent_dir+"/E_wordid-word_mapping.dat"
 h_file=sent_dir+"/H_wordid-word_mapping.dat"
+log_file = sent_dir+"/All_Resources_id_word.log"
+if os.path.exists(log_file) :
+   os.remove(log_file)
+log = open(log_file,'a')
 def create_dict(filename,string):
     with open(filename,"r") as f1: 
         text = f1.read().split("\n")
@@ -38,7 +42,7 @@ def eng_id_to_idword_pair_hash():
     except FileNotFoundError:
         print("FILE MISSING: " + e_file )
         log.write("FILE MISSING: " + e_file + "\n")
-    
+        sys.exit(0)
 #     print(e2w)
     return show_eng
     
@@ -61,7 +65,7 @@ def hin_id_to_idword_pair_hash():
     except FileNotFoundError:
         print("FILE MISSING: " + h_file )
         log.write("FILE MISSING: " + h_file + "\n")
-    
+        sys.exit(0)
 #     print(e2w)
     return show_hin
 hin_id_to_idword_pair=hin_id_to_idword_pair_hash()
@@ -69,19 +73,26 @@ hin_id_to_idword_pair=hin_id_to_idword_pair_hash()
 
 def reading_all_resources():
     all_data=[]
-    with open(sent_dir+'/All_Resources.csv','rt')as f: ####PATH TO BE CHANGED
-        data = csv.reader(f)
-        rows=list(data)
+    try :
+        with open(sent_dir+'/All_Resources.csv','rt')as f: ####PATH TO BE CHANGED
+             data = csv.reader(f)
+             rows=list(data)
+    except :
+        log.write(sent_dir+"/All_Resources.csv is missing")
+        print(sent_dir+"/All_Resources.csv is missing")
+        sys.exit(0)
     return rows
 all_resources=reading_all_resources()
 # print(all_resources)
 def E_id_conversion():
     row0=[]
     row0.append("English_Word")
-    print(row0)
+    #print(row0)
+    print(all_resources[0][1:])
     for i in all_resources[0][1:] :
 #         print(eng_id_to_idword_pair[i])
         row0.append(eng_id_to_idword_pair[int(i)])
+    print(row0)
     return row0
 row0=E_id_conversion()
 def H_id_conversion() :
@@ -95,6 +106,16 @@ def H_id_conversion() :
                 if "/" not in str(j):
                     if " " not in str(j):
                         temp_list.append(hin_id_to_idword_pair[int(j)])  
+                    elif " " in str(j) :
+                        j=str(j).split(" ")
+                        temp=[]
+                        for  k in j :
+                                if "/" not in k :
+                                      temp.append((hin_id_to_idword_pair[int(k)]))
+                                else :
+                                      k=str(k).split("/")
+                                      temp.append("/".join(hin_id_to_idword_pair[int(z)] for z in k ))
+                        temp_list.append(" ".join(temp))
                 elif "/" in str(j):
                     j=str(j).split("/")
                     temp_list.append("/".join(hin_id_to_idword_pair[int(k)] for k in j))
@@ -103,9 +124,7 @@ def H_id_conversion() :
             elif ")" in str(j) :
                 j=str(j).strip(")").split(" ")
                 temp_list.append(" ".join(hin_id_to_idword_pair[int(k)] for k in j)+")")
-            elif " " in str(j) :
-                j=str(j).split(" ")
-                temp_list.append(" ".join(hin_id_to_idword_pair[int(k)] for k in j))
+            
             else :
                 temp_list.append('0')
         all_rows.append(temp_list)
