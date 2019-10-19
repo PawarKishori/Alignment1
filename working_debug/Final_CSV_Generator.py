e6f4771a922445ba2b5f7f8192334a95a145033b
nupur-thakur <nupur@nupur-HP-Pavilion-Laptop-15-cc1xx>
2019-10-19 22:05:31 +0530

    Changes in 2 Files : All_Resources_id_word.py and Final_CSV_Generator.py

--- working_debug/Final_CSV_Generator.py
+++ working_debug/Final_CSV_Generator.py
@@ -7,22 +7,30 @@
 import csv,sys, os
 tmp_path=os.getenv('HOME_anu_tmp')+'/tmp/'
 
-#eng_file_name = 'ai1E'
-#sent_no='2.5'
+# eng_file_name = 'ai2E'
+# sent_no='2.20'
 eng_file_name = sys.argv[1]
 sent_no = sys.argv[2]
 sent_dir = tmp_path + eng_file_name + "_tmp/" + sent_no
-
+log_file = sent_dir+"/Final_CSV_Generator.log"
+if os.path.exists(log_file) :
+    os.remove(log_file)
+log = open(log_file,'a')
 def reading_new_N1():
-    all_data=[]
-    with open(sent_dir+'/new_N1.csv','rt')as f: ####PATH TO BE CHANGED
-        data = csv.reader(f)
-        rows=list(data)
-    return rows[-1]
+    try:
+        with open(sent_dir+'/new_N1.csv','rt')as f: ####PATH TO BE CHANGED
+            data = csv.reader(f)
+            rows=list(data)
+        return rows[-1]
+    except :
+        print("new_N1.csv is absent in "+sent_no)
+        log.write("new_N1.csv is absent in "+sent_no)
+        sys.exit(0)
+    
 N1_Layer=reading_new_N1()
 # print(N1_Layer)
 N1_Layer.insert(0,"N1_Layer")
-# print(N1_Layer)
+print(N1_Layer)
 print("#############################################################")
 def final_csv_creation():
     with open(sent_dir+'/All_Resources.csv','a')as f1:
