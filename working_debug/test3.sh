eb957f9fecb0145f0b38812a5561e0926e2b437c
PawarKishori <kishori.pawar@research.iiit.ac.in>
2019-10-19 12:35:59 +0530

    all anchor programs in correct sequence

--- working_debug/test3.sh
+++ working_debug/test3.sh
@@ -14,6 +14,7 @@ while [ "$i" -lt $END ]
 		source activate py3.6
 		python $HOME_alignment/working_debug/All_Resources.py $filename "2."$i
 		python $HOME_alignment/working_debug/Deciding_Anchor.py $filename "2."$i
+		python $HOME_alignment/working_debug/All_Resources_id_word.py $filename "2."$i
 		conda deactivate
 
 		i=`expr $i + 1`
