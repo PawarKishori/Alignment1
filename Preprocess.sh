29eef46ef6af31513102fd4dd015597c3ab4bf28
PawarKishori <kishori.pawar@research.iiit.ac.in>
2019-10-19 12:41:57 +0530

    segregating preprocess programs and lookup programs in 2 shelle

--- Preprocess.sh
+++ Preprocess.sh
@@ -1,5 +1,5 @@
 ##Bringing out eng-hindi dictionary entries once from Bharatwani for a Domain's parallel corpora (eg. ALL PARALLEL AI CHAPTERS).
-#sh $HOME_alignment/tech_dict/run_tech-dict_first_run.sh AI_all_eng AI_all_hin dictionary/Final_Computer_Science_Glossary_English-Hindi dictionary/Lookup_dict_ai
+sh $HOME_alignment/tech_dict/run_tech-dict_first_run.sh AI_all_eng AI_all_hin dictionary/Final_Computer_Science_Glossary_English-Hindi dictionary/Lookup_dict_ai
 
 ## following command not working.. check why? Need to debug $HOME_alignment/tech_dict/Technical_Dictionary_Integration_first_run.py
 ##sh $HOME_alignment/tech_dict/run_tech-dict_first_run.sh GeoE GeoH dictionary/technical_geography_dictionary dictionary/Lookup_dict_geography
