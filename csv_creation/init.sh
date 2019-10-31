e358d037520172c06209af6294af62caf4ff91ae
PawarKishori <kishori.pawar@research.iiit.ac.in>
2019-10-31 17:09:00 +0530

    resolved a bug and handled few cases

--- csv_creation/init.sh
+++ csv_creation/init.sh
@@ -12,6 +12,9 @@ do
     cp $HOME_alignment/csv_creation/extract_anu_root.py $HOME_anu_tmp/tmp/$2_tmp/2.$a
     cp $HOME_alignment/csv_creation/extract_hindi_id_root.py $HOME_anu_tmp/tmp/$2_tmp/2.$a
     cp $HOME_alignment/csv_creation/check_proper_noun_mng.py $HOME_anu_tmp/tmp/$2_tmp/2.$a
+    cp $HOME_alignment/csv_creation/mwe_tech_dic.txt $HOME_anu_tmp/tmp/$2_tmp/2.$a
+    cp $HOME_alignment/csv_creation/align_mwe.py $HOME_anu_tmp/tmp/$2_tmp/2.$a
+    cp $HOME_alignment/csv_creation/functions.py $HOME_anu_tmp/tmp/$2_tmp/2.$a
     
     echo "HELLO"
     cd $HOME_anu_tmp/tmp/$2_tmp/2.$a
@@ -20,14 +23,14 @@ do
     python extract_anu_root.py id_Apertium_input.dat > anu_root.dat
     python3 extract_hindi_id_root.py 
     
-     python3 csv_format.py
+    python3 csv_format.py
     
     python3 check_proper_noun_mng.py $HOME_anu_test/Anu_data/canonical_form_dictionary/dictionaries/computer_science_dic_in_canonical_form.txt  $HOME_anu_test/Anu_data/canonical_form_dictionary/dictionaries/default-iit-bombay-shabdanjali-dic_smt.txt
     
     python3 csv_parserid_to_wordid.py
     python3 sentence_details_word.py
     python3 csv_word.py
-
+    python align_mwe.py multi_word_expressions.dat mwe_tech_dic.txt 
 
     rm sent_details.py
     rm csv_format.py
@@ -36,10 +39,15 @@ do
     rm csv_word.py
     rm extract_anu_root.py
     rm extract_hindi_id_root.py
+    rm functions.py
+    rm align_mwe.py
+    rm mwe_tech_dic.txt
+
     #rm vibhakti
     #cd ../../shell
     cd $current
 #	# Print the values
     echo $a
     a=`expr $a + 1`
+
 done
