b821e02422bea1a1408de04ce541223dec81a0e2
PawarKishori <kishori.pawar@research.iiit.ac.in>
2019-11-05 23:26:46 +0530

    update

--- csv_creation/init.sh
+++ csv_creation/init.sh
@@ -2,6 +2,10 @@ a=1
 current=`pwd`
 while [ $a -lt $1 ]
 do
+    
+    
+
+
     cp $HOME_alignment/csv_creation/sent_details.py $HOME_anu_tmp/tmp/$2_tmp/2.$a
     
     cp $HOME_alignment/csv_creation/csv_format.py $HOME_anu_tmp/tmp/$2_tmp/2.$a
@@ -16,6 +20,7 @@ do
     cp $HOME_alignment/csv_creation/align_mwe.py $HOME_anu_tmp/tmp/$2_tmp/2.$a
     cp $HOME_alignment/csv_creation/functions.py $HOME_anu_tmp/tmp/$2_tmp/2.$a
     
+
     echo "HELLO"
     cd $HOME_anu_tmp/tmp/$2_tmp/2.$a
     python3 sent_details.py
