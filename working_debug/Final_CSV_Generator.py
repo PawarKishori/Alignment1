6a74c3533ac96802c9b54f11ffa6d2ce31dcac62
nupur-thakur <nupur@nupur-HP-Pavilion-Laptop-15-cc1xx>
2019-11-08 13:21:00 +0530

    Change in Final_CSV_Generator.py to integrate Apratim's Module Changes

--- working_debug/Final_CSV_Generator.py
+++ working_debug/Final_CSV_Generator.py
@@ -16,7 +16,7 @@ log_file = sent_dir+"/Final_CSV_Generator.log"
 if os.path.exists(log_file) :
     os.remove(log_file)
 log = open(log_file,'a')
-def reading_new_N1():
+def reading_N1():
     try:
         with open(sent_dir+'/new_N1.csv','rt')as f: ####PATH TO BE CHANGED
             data = csv.reader(f)
@@ -27,7 +27,7 @@ def reading_new_N1():
         log.write("new_N1.csv is absent in "+sent_no)
         sys.exit(0)
     
-N1_Layer=reading_new_N1()
+N1_Layer=reading_N1()
 # print(N1_Layer)
 N1_Layer.insert(0,"N1_Layer")
 print(N1_Layer)
