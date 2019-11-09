224324bd568eae2a0829ede2c96c431e512e78f7
PawarKishori <kishori.pawar@research.iiit.ac.in>
2019-11-09 11:31:30 +0530

    combination of Roja's module

---
+++ run_K_layer.sh
@@ -0,0 +1,8 @@
+sh $HOME_alignment/csv_creation/create_html_csv.sh $1
+cd $HOME_anu_tmp/tmp/$1_tmp
+while read line 
+do
+   cd $line
+        bash $HOME_alignment/csv_creation/run_K_layer_alignment.sh
+   cd ..
+done < dir_names.txt
