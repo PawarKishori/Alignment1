58a71df6e52c33f6ea38ee6e7959138a790958b1
PawarKishori <kishori.pawar@research.iiit.ac.in>
2019-11-09 09:50:19 +0530

    progs for 2nd run Transliteration

--- Transliteration/Transliteration_Second_Run-shell.sh
+++ Transliteration/Transliteration_Second_Run-shell.sh
@@ -5,6 +5,6 @@ do
         sentence_dir='2.'$i
         echo $sentence_dir
         tmp_path=$HOME_anu_tmp/tmp/$1_tmp/$sentence_dir
-        python3 $HOME_alignment/Transliteration/Transliteration_Second_Run.py  $tmp_path/E_sentence $tmp_path/H_sentence #path to be changed acc to your file location
+        python3 $HOME_alignment/Transliteration/Transliteration_Second_Run.py  $tmp_path/english_leftover_words.dat $tmp_path/hindi_leftover_words.dat #path to be changed acc to your file location
         i=`expr $i + 1`
 done
