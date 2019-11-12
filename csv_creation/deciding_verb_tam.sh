4eacdd006dd32df9fb53187e508d8965ad234963
PawarKishori <kishori.pawar@research.iiit.ac.in>
2019-11-12 12:13:56 +0530

    updates

--- csv_creation/deciding_verb_tam.sh
+++ csv_creation/deciding_verb_tam.sh
@@ -10,9 +10,13 @@ do
         echo $sentence_dir
         tmp_path=$HOME_anu_tmp/tmp/$1_tmp/$sentence_dir
 
-python3 $HOME_alignment/csv_creation/get_manual_root_nd_tam.py $tmp_path/H_wordid-word_mapping.dat $tmp_path/verb_root_tam_info  $HOME_alignment/csv_creation/kriyA_mUla.txt_wx > $tmp_path/verb_root_tam_info.dat
 
-        python3 $HOME_alignment/csv_creation/verb_alignment.py  $tmp_path/verb_root_tam_info.dat $tmp_path/anu_root.dat
+#python3 $HOME_alignment/csv_creation/get_manual_root_nd_tam.py H_wordid-word_mapping.dat verb_root_tam_info $HOME_alignment/csv_creation/kriyA_mUla_default_dic.txt  $HOME_alignment/csv_creation/kriyA_mUla.txt_wx > verb_root_tam_info.dat
+
+cd $tmp_path
+python3 $HOME_alignment/csv_creation/get_manual_root_nd_tam.py $tmp_path/H_wordid-word_mapping.dat $tmp_path/verb_root_tam_info $HOME_alignment/csv_creation/kriyA_mUla_default_dic.txt  $HOME_alignment/csv_creation/kriyA_mUla.txt_wx > $tmp_path/verb_root_tam_info.dat
+
+python3 $HOME_alignment/csv_creation/verb_alignment.py  $tmp_path/verb_root_tam_info.dat $tmp_path/anu_root.dat
 
 done
 
