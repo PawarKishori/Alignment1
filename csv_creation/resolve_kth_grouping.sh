a090e1102f82afa9f5d841612ada1f48500b16c3
PawarKishori <kishori.pawar@research.iiit.ac.in>
2019-11-13 16:06:59 +0530

    shreya's module

--- csv_creation/resolve_kth_grouping.sh
+++ csv_creation/resolve_kth_grouping.sh
@@ -8,14 +8,20 @@ while [ "$i" -lt $END ]
   echo "2."$i
   source activate py3.6
   tmp=$HOME_anu_tmp/tmp/$1_tmp/"2."$i
-  python3 $HOME_alignment/csv_creation/making_group_using_id_Apertium_word_dat.py $tmp/word.dat $tmp/id_Apertium_output_with_grp.dat $tmp/K_grouping_info.dat
-  #python3 $HOME_alignment/csv_creation/making_list_using_grouping_information.py $tmp/srun_All_Resources_id_word.csv $tmp/id_Apertium_output_with_grp.dat $tmp/K_grouping_info.dat
-  python3 $HOME_alignment/csv_creation/meaning_of_every_word_shreya.py $tmp/K_grouping_info.dat $HOME_anu_test/Anu_data/domain/computer_science_dic.txt $tmp/resolved_kth_groups.dat
+  python3 $HOME_alignment/csv_creation/all_words_using_apertium.py $tmp/word.dat $tmp/id_Apertium_output_with_grp.dat $tmp/K_grouping_info.dat $tmp/K_grouping_info1.dat
+  
+  python3 $HOME_alignment/csv_creation/meaning_of_every_word_shreya.py $tmp/K_grouping_info1.dat $HOME_anu_test/Anu_data/domain/computer_science_dic.txt $tmp/resolved_kth_groups.dat
+
+ cut -f1 $tmp/K_grouping_info.dat > $tmp/first_column
+ cut -f2 $tmp/resolved_kth_groups.dat > $tmp/second_column
+ 
+ paste $tmp/first_column $tmp/second_column > $tmp/input_to_K_enhanced_layer
+
+ python3 $HOME_alignment/csv_creation/csv_conversion_id_final.py $tmp/input_to_K_enhanced_layer  $tmp/K_enhanced.dat
+
   conda deactivate
 
                 i=`expr $i + 1`
         done
 
 echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-
