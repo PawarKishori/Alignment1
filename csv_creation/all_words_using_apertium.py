61ce2b237fdb27ad743a1de1922434ea47bd30a6
PawarKishori <kishori.pawar@research.iiit.ac.in>
2019-11-14 19:05:24 +0530

    Shreya handled new case

--- csv_creation/all_words_using_apertium.py
+++ csv_creation/all_words_using_apertium.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[37]:
+# In[8]:
 import sys
 import string
 
@@ -26,16 +26,18 @@ def word(word_id_file):
     return eword
 
 
-def extract_groups(word_file, id_apertium):
+def extract_groups(word_file, id_Apertium_file):
         # hindi_dict_id_root={}
     array = []
     list1 = []
     list2 = []
     list3 = []
-    elist=[]
+    elist = []
     ewords = word(word_file)
     # print(ewords)
-    with open(id_apertium, "r") as f:
+    f = len(ewords)
+    # print(f)
+    with open(id_Apertium_file, "r") as f:
         data = f.read().split("\n")
         # print(array)
         for i in data:
@@ -50,32 +52,35 @@ def extract_groups(word_file, id_apertium):
         list2 = list2[:-1]
         # print(list2)
         final = []
-        final2=[]
+        final2 = []
         # print(list2)
         for i in list2:
             eng = ""
+            hin = ""
             for j in range(0, 1):
-                if "@PUNCT-OpenParen@PUNCT-OpenParen - )" in i[1] or "@PUNCT-OpenParen@PUNCT-OpenParen -- )" in i[1]:
+                if "@PUNCT-OpenParen@PUNCT-OpenParen" in i[1]:
                     c = i[0]
                     # print(c)
-                    l=len(" @PUNCT-ClosedParen@PUNCT-ClosedParen )")
+                    l = len(" @PUNCT-ClosedParen@PUNCT-ClosedParen )")
+                    hin = i[1].strip("@PUNCT-OpenParen@PUNCT-OpenParen").strip(
+                        "@PUNCT-OpenParen@PUNCT-OpenParen - )").strip("@PUNCT-OpenParen@PUNCT-OpenParen -- )")
                     d = list2.index(i)
                     m = list2[d+1][0]
-                    # print(list2[d+1][1])
-                    str1 = list2[d+1][1]
-                    hword=str1[:-l]
-                    # print(hword)
-                    hword = hword.strip(" ")
+                    str1 = hin+list2[d+1][1]
+                    hword = str1[:-l]
+                    hword = hword.strip(" ").strip("-")
                     hword = hword.replace(" ", "_")
                     for k in range(int(c)-1, int(m)):
                         eng = eng+ewords[k]+"_"
                         elist.append(k+1)
+                        # print(k)
                     eng = eng.strip("_")
                     final2.append(eng+"\t"+hword)
-                    x=k-(int(m)-int(c))+1    
-                    eng=str(x)+"_"+eng
-                    
+                    x = k-(int(m)-int(c))+1
+                    eng = str(x)+"_"+eng
+
                     final.append(eng+"\t"+hword)
+
                 elif " @PUNCT-ClosedParen@PUNCT-ClosedParen )" not in i[1] and "@PUNCT-OpenParen@PUNCT-OpenParen - )" not in i[1] and "@PUNCT-OpenParen@PUNCT-OpenParen -- )" not in i[1]:
                     c = i[0]
                     # print(i)
@@ -83,33 +88,36 @@ def extract_groups(word_file, id_apertium):
                     # print(d)
                     g = int(list2[d][0])
                     # print(g)
-                    # print(ewords(g))
                     elist.append(g)
+                    # print(ewords(g))
                     hword = list2[d][1].strip(" )")
                     if hword is "":
-                        hword="0"
+                        hword = "0"
                     final2.append(ewords[g-1]+"\t"+hword)
                     final.append(str(g)+"_"+ewords[g-1]+"\t"+hword)
-        for i in range(1,len(ewords)+1):
+        for i in range(1, len(ewords)+1):
             if i not in elist:
                 final2.append(ewords[i-1]+"\t"+"0")
-                final.append(str(i)+"_"+ewords[i-1]+"\t"+"0")      
-        return final,final2
+                final.append(str(i)+"_"+ewords[i-1]+"\t"+"0")
+        return final, final2
 
 
         # print(final)
 input_dict = sys.argv[1]
 input_dict1 = sys.argv[2]
 output_dict = sys.argv[3]
-output_dict1=sys.argv[4]
+output_dict1 = sys.argv[4]
 # converting_dictionaries_into_list()
-output1,output2 = extract_groups(input_dict, input_dict1)
+output1, output2 = extract_groups(input_dict, input_dict1)
 with open(output_dict, "w") as out:
     for i in output1:
         out.write(i+"\n")
-with open(output_dict1,"w") as out1:
+with open(output_dict1, "w") as out1:
     for j in output2:
         out1.write(j+"\n")
 
 
 # extract_dictionary_ordered_fact()
+
+
+# In[ ]:
