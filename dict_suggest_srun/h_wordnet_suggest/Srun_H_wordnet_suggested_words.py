fc06baa5fe83df83b5ca90b6ae7b9f6298c2cddf
Saumya <saumya@anu>
2019-11-18 15:59:38 +0530

    Updated Code

--- dict_suggest_srun/h_wordnet_suggest/Srun_H_wordnet_suggested_words.py
+++ dict_suggest_srun/h_wordnet_suggest/Srun_H_wordnet_suggested_words.py
@@ -4,41 +4,49 @@ import sys
 import glob
 
 def obtaining_mapped_words(folder):
-    input_file=open(folder+"/anu_root.dat",'r')
-    eng_word_mapping=open(folder+'/E_wordid-word_mapping.dat','r')
-    per_line_input=input_file.readlines()
-    file_list=[]
-    file_list_2=[]
-    split_list=[]
-    element=[]
-    required={}
-    for i in per_line_input:
-        file_list.append(i.split("  "))
-    for i in file_list:
-        element.append(i[1][:-3].split(" "))
+    try:
+        input_file=open(folder+"/anu_root.dat",'r')
+        eng_word_mapping=open(folder+'/E_wordid-word_mapping.dat','r')
+        per_line_input=input_file.readlines()
+        file_list=[]
+        file_list_2=[]
+        split_list=[]
+        element=[]
+        element1=[]
+        required={}
 
+        for i in per_line_input:
+            file_list.append(i.split(" "))
 
-    per_line_mapping=eng_word_mapping.readlines()
-    
-    for i in per_line_mapping:
-        split_list.append(i.split("\t"))
-    
-    for i in split_list:
-        required[i[1]]=i[2][:-2]
-    final_list=[]
-    for i in element:
-        
-        for key in required:
-            list_list=[]
-            if key == i[0]:
-                
-                i[0]=required[key]
-                list_list.append(i[0])
-                list_list.append(i[1])
-            if len(list_list)!=0:
-            	final_list.append(list_list)
-      
-    return(final_list)
+        for i in file_list:
+            element1=[]
+            element1.append(i[1])
+            element1.append(i[2])
+            element.append(element1)
+
+        per_line_mapping=eng_word_mapping.readlines()
+
+        for i in per_line_mapping:
+            split_list.append(i.split("\t"))
+
+        for i in split_list:
+            required[i[1]]=i[2][:-2]
+        final_list=[]
+        for i in element:
+
+            for key in required:
+                list_list=[]
+                if key == i[0]:
+
+                    i[0]=required[key]
+                    list_list.append(i[0])
+                    list_list.append(i[1])
+                if len(list_list)!=0:
+                    final_list.append(list_list)
+
+        return(final_list)
+    except:
+        print(folder," missing files")
 def process(eng,hin,sentence,folder):
     
     
@@ -50,75 +58,72 @@ def process(eng,hin,sentence,folder):
     adv=hnd_wordnet_adv.readlines()
     noun=hnd_wordnet_noun.readlines()
     verb=hnd_wordnet_verb.readlines()
-    adj_list=[]
-    adv_list=[]
-    noun_list=[]
-    verb_list=[]
+    adj_dict={}
+    adv_dict={}
+    noun_dict={}
+    verb_dict={}
     outcome=[]
+    list_split_1=[]
+    list_split_2=[]
+    list_split_3=[]
+    list_split_4=[]
     for i in adj: 
-        adj_sublist=re.split(r'\t+', i)
-        adj_list.append(adj_sublist)
-    for i in adv:
-        adv_sublist=re.split(r'\t+', i)
-        adv_list.append(adv_sublist)
-    for i in noun:
-        noun_sublist=re.split(r'\t+', i)
-        noun_list.append(noun_sublist)
-    for i in verb:
-        verb_sublist=re.split(r'\t+', i)
-        verb_list.append(verb_sublist)
-    try:
-        for i in adj_list:
-            if hin == i[0]:
-                outcome.append(i[1])
-        for j in adv_list:
-            if hin == j[0]:
-                if len(outcome)!=0: 
-                    outcome.append('/'+j[1])
-                else:
-                    outcome.append(j[1])
+        list_split_1.append(i[:-1].split('\t'))
+    for i in list_split_1:
+        adj_dict[i[0]]=i[1]
+    #print(adj_dict)
+    for i in adv: 
+        list_split_2.append(i[:-1].split('\t'))
+    for i in list_split_2:
+        adv_dict[i[0]]=i[1]
+    for i in noun: 
+        list_split_3.append(i[:-1].split('\t'))
+    for i in list_split_3:
+        noun_dict[i[0]]=i[1]
+    for i in verb: 
+        list_split_4.append(i[:-1].split('\t'))
+    for i in list_split_4:
+        verb_dict[i[0]]=i[1]
 
-        for k in noun_list:
-            if hin == k[0]:
-                if len(outcome)!=0: 
-                    outcome.append('/'+k[1])
-                else:
-                    outcome.append(k[1])
-        for w in verb_list:
-            if hin == w[0]:
-                if len(outcome)!=0: 
-                    outcome.append('/'+w[1])
-                else:
-                    outcome.append(w[1])
-        #print("outcome",outcome)
+    try:
+        for key,value in adj_dict.items():
+            if hin == key:
+                outcome.append(value)
+
+        for key,value in adv_dict.items():
+            if hin == key:
+                outcome.append(value)
+
+        for key,value in noun_dict.items():
+            if hin == key:
+                outcome.append(value)
+
+        for key,value in verb_dict.items():
+            if hin == key:
+                outcome.append(value)
+
         words=outcome[0].split('/')
         for i in words:
-            file_write=open(folder+"/srun_H_wordnet.dat",'a')
             if i in sentence:
-                print(eng +" <> "+i+'\n')
+                print(eng +" <> "+i)
                 file_write.write(eng +" <> "+i+'\n')
         file_write.close()
     except:
-        #print("no corresponding word")
         pass
             
-            
-# sentence1=open("/home/user/tmp_anu_dir/tmp/ai1E_tmp/2.68/hindi_leftover_words.dat",'r').readline()
 tmp_path = os.getenv('HOME_anu_tmp')+'/tmp/'
 path = tmp_path + sys.argv[1]+'_tmp/2.*'
 folders = sorted(glob.glob(path))
 
-# for files in each folder
 for folder in folders:
     try:
+        print(folder)
+
         file_write=open(str(folder)+"/srun_H_wordnet.dat",'w')
         file_write.close()
         sentence1=open(str(folder)+"/hindi_leftover_words.dat",'r').readline()
         sentence=sentence1.split(" ")
-        #print(sentence)
         element=obtaining_mapped_words(str(folder))
-        #print(str(folder))
-        #print(sentence)
         for i in element:
             hin=i[1]
             eng=i[0]
