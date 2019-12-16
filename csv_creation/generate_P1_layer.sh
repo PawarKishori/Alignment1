6867e580f2c7330fd28e6728c8452a11e124f43e
roja <roja19p@gmail.com>
2019-12-16 16:21:25 +0530

    Updated generate_P1_layer.sh

--- csv_creation/generate_P1_layer.sh
+++ csv_creation/generate_P1_layer.sh
@@ -44,7 +44,7 @@ wx_utf8 < H_sentence_with_ids.dat > H_sentence_with_ids_utf8.dat
 python3 $HOME_alignment/csv_creation/group.py E_grouping.dat > E_grouping.tsv
 python3 $HOME_alignment/csv_creation/group.py H_grouping.dat > H_grouping.tsv
 
-sed 's/, /\t/g' srun_All_Resources_id_word.csv > srun_All_Resources_id_word.tsv
+sed 's/,/\t/g' srun_All_Resources_id_word.csv > srun_All_Resources_id_word.tsv
 
 cat H_sentence_with_ids_utf8.dat E_grouping.tsv H_grouping.tsv srun_All_Resources_id_word.tsv > complete_alignment.tsv
 
